f5ddea35ee956bcbe5c901db6c269f7ccba00096
DumbCaveSpider <markiel_giles@outlook.com>
2025-09-28 14:19:07 +0800

    android test

--- src/android.cpp
+++ src/android.cpp
@@ -1,51 +1,48 @@
 #if defined(__ANDROID__)
-#include <jni.h>
-#include <android/native_activity.h>
-#include <android/log.h>
-
 #include <Geode/cocos/platform/android/jni/JniHelper.h>
+#include <jni.h>
 
 namespace battery {
 
-
 int getBatteryLevel() {
-    cocos2d::JniMethodInfo methodInfo;
-    int batteryLevel = -1;
-    if (cocos2d::JniHelper::getStaticMethodInfo(methodInfo,
+    cocos2d::JniMethodInfo mi;
+    int level = -1;
+    if (cocos2d::JniHelper::getStaticMethodInfo(mi,
         "org/cocos2dx/lib/Cocos2dxHelper",
         "getBatteryLevel",
         "()I")) {
-        batteryLevel = methodInfo.env->CallStaticIntMethod(methodInfo.classID, methodInfo.methodID);
-        methodInfo.env->DeleteLocalRef(methodInfo.classID);
+        level = mi.env->CallStaticIntMethod(mi.classID, mi.methodID);
+        mi.env->DeleteLocalRef(mi.classID);
     }
-    return batteryLevel;
+    return level;
 }
 
 bool isCharging() {
-    cocos2d::JniMethodInfo methodInfo;
+    cocos2d::JniMethodInfo mi;
     bool charging = false;
-    if (cocos2d::JniHelper::getStaticMethodInfo(methodInfo,
+    if (cocos2d::JniHelper::getStaticMethodInfo(mi,
         "org/cocos2dx/lib/Cocos2dxHelper",
         "isCharging",
         "()Z")) {
-        charging = methodInfo.env->CallStaticBooleanMethod(methodInfo.classID, methodInfo.methodID);
-        methodInfo.env->DeleteLocalRef(methodInfo.classID);
+        charging = mi.env->CallStaticBooleanMethod(mi.classID, mi.methodID);
+        mi.env->DeleteLocalRef(mi.classID);
     }
     return charging;
 }
 
 bool isBatterySaver() {
-    cocos2d::JniMethodInfo methodInfo;
+    cocos2d::JniMethodInfo mi;
     bool saver = false;
-    if (cocos2d::JniHelper::getStaticMethodInfo(methodInfo,
+    if (cocos2d::JniHelper::getStaticMethodInfo(mi,
         "org/cocos2dx/lib/Cocos2dxHelper",
         "isBatterySaver",
         "()Z")) {
-        saver = methodInfo.env->CallStaticBooleanMethod(methodInfo.classID, methodInfo.methodID);
-        methodInfo.env->DeleteLocalRef(methodInfo.classID);
+        saver = mi.env->CallStaticBooleanMethod(mi.classID, mi.methodID);
+        mi.env->DeleteLocalRef(mi.classID);
     }
     return saver;
 }
 
 }
+
 #endif
